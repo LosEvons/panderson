7202177c1c53913993f218a83642cbd688a472e2
LosEvons <emil.malk@gmail.com>
2026-03-10 22:59:01 +0200

    Implemented basic chess stuff

--- src/panderson/chess.clj
+++ src/panderson/chess.clj
@@ -1,4 +1,4 @@
-(ns panderson.chess 
+(ns panderson.chess
   (:require
    [clojure.string :as string]))
 
@@ -23,13 +23,18 @@
 
 ; Board setup
 (defn initial-board []
-  (let [back-rank [white-rook white-knight white-bishop white-queen 
+  (let [back-rank [white-rook white-knight white-bishop white-queen
                    white-king white-bishop white-knight white-rook]
-        white-back (into {} (map-indexed (fn [f p] [[0 f] p]) back-rank))
-        white-pawns (into {} (for [f (range 8)] [[1 f] white-pawn]))
-        black-back (into {} (map-indexed (fn [f p] [[7 f] (assoc p :color :black)]) back-rank))
-        black-pawns (into {} (for [f (range 8)] [[6 f] black-pawn]))]
-    (merge white-back white-pawns black-back black-pawns))
+        white-back (into {} (map-indexed (fn [i p] [[0 i] p]) back-rank))
+        white-pawns (into {} (for [i (range 8)] [[1 i] white-pawn]))
+        black-back (into {} (map-indexed (fn [i p] [[7 i] (assoc p :color :black)]) back-rank))
+        black-pawns (into {} (for [i (range 8)] [[6 i] black-pawn]))]
+    (merge white-back white-pawns black-back black-pawns)))
+
+(comment
+  (def b (initial-board))
+  (count b)
+  (get b [1 4]) 
   )
 
 ; Print board
@@ -51,3 +56,50 @@
     (doseq [file (range 8)]
       (print (if-let [p (get board [rank file])] (piece->str p) ".")))
     (println)))
+
+(comment
+  (print-board b) 
+  )
+
+; Coordinate utils
+(defn bounds-check [[rank file]] (and (<= 0 rank 7) (<= 0 file 7)))
+(defn square-empty-check [board square] (nil? (get board square)))
+(defn square-color [board square] (get-in board [square :color]))
+
+; Piece move rules
+(defn pawn-moves [board [rank file :as square]]
+  (let [piece (get board square)
+        color (:color piece)
+        forward (if (= color :white) 1 -1)
+        one-step [(+ rank forward) file]
+        two-step [(+ rank (* 2 forward)) file]]
+    (when piece
+      (cond->[]
+       (and (bounds-check one-step) (square-empty-check board one-step))
+        (conj {:from square :to one-step})
+        (and (bounds-check two-step)
+             (= rank (if (= color :white) 1 6))
+             (square-empty-check board one-step)
+             (square-empty-check board two-step))
+        (conj {:from square :to two-step})))))
+
+; Move action
+(defn moves-for-square [board square]
+  (when-let [piece (get board square)]
+        (case (:type piece)
+          :pawn (pawn-moves board square)
+          ;; add moves
+          [])))
+
+
+(defn move [board {:keys [from to]}]
+  (-> board
+      (dissoc from)
+      (assoc to (get board from))))
+
+(comment
+  (moves-for-square b [1 4])
+  (def b2 (move b {:from [1 4] :to [3 4]}))
+  (get b [1 4])
+  (get b2 [1 4])
+  )
